eaf93b0c937137e0a06fbeea4d4bf8c59f0f4d28
Shahzad-Minhaj <sminhaj.suit@gmail.com>
2024-02-11 12:43:43 +0500

    Updated on 11-Feb

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 
-// let array = ["play", 2, 3, 4, 5];
-// let elementToRemove = "play";
-// array = array.filter(item => item !== elementToRemove);
-// console.log(array)
-
-
 let player1Score = 0;
 let player2Score = 0;
 let currentPlayer = 1;
 let gameActive = true;
-let array = ["player1ScoreStartedOnSix", "player2ScoreStartedOnSix"]
+let array = ["1","2"]
+document.getElementById("player1").classList.add("active-player");
+
+//========================================================================
+//                  Clicking button Roll function
+//========================================================================
 
 document.getElementById("roll").onclick = function () {
     if (!gameActive) return
@@ -17,77 +16,156 @@ document.getElementById("roll").onclick = function () {
     console.log("Dice Roll: " + x);
     document.getElementById("output3").innerHTML = x;
     document.getElementById("theImg").src = `/images/dice-${x}.png`;
-    let checks1 = () => {
 
-        for (let i = 0; i < array.length; i++) {
-
-            if (array[i] == "player1ScoreStartedOnSix") {
-                array.splice(i, 1);
-                currentPlayer = 1
-                player1Score += x;
-                document.getElementById("player1Score").innerHTML = player1Score;
-                console.log("check1_if", x, array)
-            }
-            else {
-                currentPlayer = 1
-                player1Score += x;
-                document.getElementById("player1Score").innerHTML = player1Score;
-                console.log("check1_else", x, array)
-
-            }
-        }
-    }
-    let checks2 = () => {
-        for (let i = 0; i < array.length; i++) {
-            if (array[i] == "player2ScoreStartedOnSix") {
-                array.splice(i, 1);
-                currentPlayer = 2
-                console.log("check2_if", x, array)
-                player2Score += x;
-                document.getElementById("player2Score").innerHTML = player2Score;
-            }
-            else if (array.length == 0) {
-
-
-            }
-            else {
-                currentPlayer = 2
-                console.log("check2_else", x, array)
-                player2Score += x;
-                document.getElementById("player2Score").innerHTML = player2Score;
-
-            }
-        }
-    }
-
-    if (currentPlayer == 1 && x == 6) {
-        checks1()
-
-    }
-    else if (currentPlayer == 2 && x == 6) {
-        checks2()
-    }
-    else if (currentPlayer == 1 && x != 6 && !array.includes("player1ScoreStartedOnSix")) {
-        currentPlayer = 2
+    let scoreUpdate1=()=>{
         player1Score += x
-        document.getElementById("player1Score").innerHTML = player1Score;
-        console.log("elseif player 1")
+        document.getElementById("player1Score").innerHTML = player1Score
     }
-    else if (currentPlayer == 2 && x != 6 && !array.includes("player2ScoreStartedOnSix")) {
-        currentPlayer = 1
+
+    let scoreUpdate2=()=>{
         player2Score += x
-        document.getElementById("player2Score").innerHTML = player2Score;
-                console.log("elseif player 2")
+        document.getElementById("player2Score").innerHTML = player2Score
+
     }
+
+    //===================(Game logic for player # 1)================================
+    /* cp= currentPlayer, np= newPlayer, x is dice outcome and array is used to check the status of
+    6 on the dice once for both players to start the score on scoreboard */
+
+    if(currentPlayer == 1 && x == 6 && array.includes("1")){
+        document.getElementById("player1").classList.add("active-player");
+        array.shift()
+        currentPlayer =1
+        scoreUpdate1()
+        console.log("d",x,"cp =1, x=6 arrayinclude1", array,"np",currentPlayer)
+    }
+    else if(currentPlayer ==1 && x == 6 && !array.includes("1")){
+        document.getElementById("player1").classList.add("active-player");
+        currentPlayer =1
+        scoreUpdate1()
+        console.log("dice",x,"cp=1 =, x=6 !arrayinclude1", array,"np",currentPlayer)
+    }
+    else if( currentPlayer == 1 && x!=6 && array.includes("1")){
+        document.getElementById("player2").classList.add("active-player");
+        document.getElementById("player1").classList.remove("active-player");
+        currentPlayer =2
+        console.log("dice",x,"cp =1, x!=6 arrayinclude1", array,"np",currentPlayer)
+    }
+    else if( currentPlayer ==1 && x!=6 && !array.includes("1")){
+        document.getElementById("player1").classList.remove("active-player");
+        document.getElementById("player2").classList.add("active-player");
+        currentPlayer =2
+        scoreUpdate1()
+        console.log("dice",x,"cp =1, x=!6, !arrayinclude1", array,"np",currentPlayer)
+    }
+
+        //===================(Game logic for player # 2)================================
+
+
+    else if(currentPlayer == 2 && x == 6 && array.includes("2")){
+        document.getElementById("player2").classList.add("active-player")
+        array.pop()
+        currentPlayer =2
+        scoreUpdate2()
+        console.log("dice",x,"cp =2, x=6 arrayinclude2", array,"np",currentPlayer)
+    }
+    else if(currentPlayer ==2 && x == 6 && !array.includes("2")){
+        document.getElementById("player2").classList.add("active-player")
+        currentPlayer =2
+        scoreUpdate2()
+        console.log("dice",x,"cp =2, x=6, !arrayinclude2", array,"np",currentPlayer)
+    }
+    else if( currentPlayer == 2 && x!= 6 && array.includes("2")){
+        document.getElementById("player1").classList.add("active-player")
+        document.getElementById("player2").classList.remove("active-player");
+        currentPlayer =1
+        console.log("dice",x,"cp =2, x=!6, arrayinclude2", array,"np",currentPlayer)
+    }
+    else{
+        document.getElementById("player1").classList.add("active-player")
+        document.getElementById("player2").classList.remove("active-player");
+
+        currentPlayer =1
+        scoreUpdate2()
+        console.log("dice",x,"cp =2, x!=6, !arrayinclude2", array,"np",currentPlayer)
+    }
+}
+//===================(End of the Roll function)=====================================
+
+
+//----------------------------------------(Second attempt)------------------------------------------------------
+// let array = ["play", 2, 3, 4, 5];
+// let elementToRemove = "play";
+// array = array.filter(item => item !== elementToRemove);
+// console.log(array)
+    //     let checks1 = () => {
+
+//         for (let i = 0; i < array.length; i++) {
+
+//             if (array[i] == "player1ScoreStartedOnSix") {
+//                 array.shift()
+//                 currentPlayer = 1
+//                 player1Score += x;
+//                 document.getElementById("player1Score").innerHTML = player1Score;
+//                 console.log("check1_if", "dice",x,"player 1 score",player1Score)
+//             }
+//             else {
+//                 currentPlayer = 1
+//                 player1Score += x;
+//                 document.getElementById("player1Score").innerHTML = player1Score;
+//                 console.log("check1_else", "dice",x, "player1",player1Score)
+
+//             }
+//         }
+//     }
+//     let checks2 = () => {
+//         for (let i = 0; i < array.length; i++) {
+//             if (array[i] == "player2ScoreStartedOnSix") {
+//                 array.pop()
+//                 currentPlayer = 2
+//                 console.log("check2_if", "dice",x, "player2", player2Score)
+//                 player2Score += x;
+//                 document.getElementById("player2Score").innerHTML = player2Score;
+//             }
+        
+//             else {
+//                 currentPlayer = 2
+//                 console.log("check2_else", "dice", x, "player2",player2Score)
+//                 player2Score += x;
+//                 document.getElementById("player2Score").innerHTML = player2Score;
+
+//             }
+//         }
+//     }
+
+//     if (currentPlayer == 1 && x == 6) {
+//         checks1()
+
+//     }
+//     else if (currentPlayer == 2 && x == 6) {
+//         checks2()
+//     }
+//     else if (currentPlayer == 1 && x != 6 && !array.includes("player1ScoreStartedOnSix")) {
+//         currentPlayer = 2
+//         player1Score += x
+//         document.getElementById("player1Score").innerHTML = player1Score;
+//         console.log("elseif player 1"," dice", x, "player1",player1Score)
+//     }
+//     else if (currentPlayer == 2 && x != 6 && !array.includes("player2ScoreStartedOnSix")) {
+//         currentPlayer = 1
+//         player2Score += x
+//         document.getElementById("player2Score").innerHTML = player2Score;
+//                 console.log("elseif player 2"," dice",x,"player2",player2Score)
+//     }
    
 
-}
+// }
 
 
 
 
 
-//------------------------------------------------------------------------------------
+//----------------------------------------------------------(first attempt)--------------------------
 // let diceRoll = () => {
 //     return Math.trunc(Math.random() * 6) + 1;
 // }
@@ -161,6 +239,10 @@ document.getElementById("roll").onclick = function () {
 //     document.getElementById("player" + currentPlayer).classList.add("active-player");
 // };
 
+//=============================================================================================
+            //      Reset button function
+//=============================================================================================
+
 document.getElementById("reset").onclick = function () {
     resetGame();
 };
@@ -170,6 +252,7 @@ function resetGame() {
     player2Score = 0;
     currentPlayer = 1;
     gameActive = true;
+    array=["1","2"]
 
     // Reset scores
     document.getElementById("player1Score").innerHTML = "0";
@@ -187,6 +270,11 @@ function resetGame() {
     document.getElementById("output3").innerHTML = "Let's Play";
     document.getElementById("theImg").src = `/images/dice-${hi}.png`;
 }
+//=====================(End of the reset button function)========================
+
+//=========================================================================================
+    //      Winner announcement
+//=========================================================================================
 
 function announceWinner(winner) {
     console.log("Game Over! " + winner + " is the winner!");
